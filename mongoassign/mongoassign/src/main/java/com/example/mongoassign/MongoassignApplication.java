860061a99899013281226ff3776fd2fec3cd6d64
Krishnakanth Kini <55691363+KrishnakanthKini@users.noreply.github.com>
2019-12-31 21:30:17 +0530

    ..

---
+++ mongoassign/mongoassign/src/main/java/com/example/mongoassign/MongoassignApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongoassign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoassignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoassignApplication.class, args);
+	}
+
+}
